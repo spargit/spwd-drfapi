8cac22192b76009159f4415869c2d91852b3c526
Shawn Parsons <mail@shawnparsons.ca>
2018-11-28 17:45:12 -0500

    Initial commit.

---
+++ drfapi/secrets.py
@@ -0,0 +1,8 @@
+DB_NAME = 'django'
+DB_USER = 'postgres'
+DB_PASS = 'postgres'
+DB_HOST = 'db_django'
+DB_PORT = 5432
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ')+7yy@i#n92f#cvo63h_cfvsi9@)f8=!8qdv(=esb$v6hs#qfx'
